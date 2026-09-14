0dd454cebd94a0d1ef22e965e3b171f59176c00b
Richard Okafor <javadevrekahdo@gmail.com>
2026-09-14 23:46:55 +0100

    fix: update Error import path casing for Linux builds

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/home/Home'
 import { HeaderProvider } from './contexts/HeaderProvider'
-import { Error } from './pages/error/Error'
+import { Error } from './pages/Error/Error'
+import { HeroProvider } from './contexts/HeroProvider'
 
 function App() {
   return (
-    <HeaderProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </HeaderProvider>
+    <HeroProvider>
+      <HeaderProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </HeaderProvider>
+    </HeroProvider>
   )
 }
 
