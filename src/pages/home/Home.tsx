089ecdb0a32cf8fcf49af64a2679d135285dd9eb
Richard Okafor <javadevrekahdo@gmail.com>
2026-09-18 08:21:46 +0100

    fix deployment

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import { HeaderProvider } from '../../contexts/HeaderProvider'
 import { HeroProvider } from '../../contexts/HeroProvider'
-import { AboutSection } from '../../sections/about/AboutSection'
+// import { AboutSection } from '../../sections/about/AboutSection'
 import { HeaderSection } from '../../sections/header/HeaderSection'
 import { HeroSection } from '../../sections/hero/HeroSection'
 
